f3f87405721fa9faf79c7813917ddf49da7ee2e5
Florian JUDITH <florian.judith.b@gmail.com>
2018-03-06 09:12:56 +0100

    removed duplicated script

--- multi-node/vagrant/init-kubectl.1.sh
+++
@@ -1,7 +0,0 @@
-export KUBECONFIG="$(pwd)/kubeconfig"
-kubectl config use-context vsphere-multi
-kubectl config set-cluster vsphere-multi-cluster --server=https://192.168.251.101:443 --certificate-authority=${PWD}/ssl/ca.pem
-kubectl config set-credentials vsphere-multi-admin --certificate-authority=${PWD}/ssl/ca.pem --client-key=${PWD}/ssl/admin-key.pem --client-certificate=${PWD}/ssl/admin.pem
-kubectl config set-context vsphere-multi --cluster=vsphere-multi-cluster --user=vsphere-multi-admin
-kubectl config use-context vsphere-multi
-kubectl get nodes
